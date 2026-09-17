e6e11e3ba250b926840a661443b727861fb885b6
Wallevve <144934719+Wallevve@users.noreply.github.com>
2026-09-17 20:09:41 +0700

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.bayu.dnsvpn"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.bayu.dnsvpn"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("com.google.android.material:material:1.13.0")
+}
